0ae1f7038693ce31c7bbe9577cf0874af307f2ca
Özgür Kara <ozgur71di@gmail.com>
2024-11-15 01:41:23 +0300

    Update ScriptManagement.cs

--- Assets/Scripts/MonoB/Game/ScriptManagement.cs
+++ Assets/Scripts/MonoB/Game/ScriptManagement.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 public class ScriptManagement : MonoBehaviour
 {
+    // Singleton design pattern:
     public static ScriptManagement Instance { get; private set; }
+    // ----------------------------------------------------------
 
     [SerializeField] private GridManager _gridManager;
     [SerializeField] private CardSpawner _cardSpawner;
     [SerializeField] private PositionHandler _positionHandler;
 
+    // Singleton design pattern:
     private void Awake()
     {
         DontDestroyOnLoad(this);
@@ -24,7 +27,8 @@ public class ScriptManagement : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    
+    // -----------------------------------------------------------------------------------------------------------
+
     public GridManager GetGridManager() { return _gridManager; }
     public CardSpawner GetCardSpawner() { return _cardSpawner; }
 
